ee8150cd94a87be773365c4e662884880d1cf723
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-06-30 08:36:00 +0000

    date time

---
+++ 01_Bascics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1242342
+
+let acountEmail = "hi@gmail.com"
+
+var acountpassword = "11"
+
+acountcity = "Jaipur"
+
+console.log(accountId);
---
+++ 01_Bascics/02_Datatype.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log(3+3)
+
+console.log("Haider")
+
+// alert(3+3)  donot use b/c we using node js
+let name = "naam"
+
+let age = "18"
+
+let loginnn = false
