3772bdf087bef40b62d14bdfc2cf8525dcb68c3e
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-02 14:21:05 +0000

    objects

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,25 @@
+
+const my_array =[0, 1, 2, 3, 4, 5]
+// // my_array.push(6)
+// // my_array.pop()
+
+// // my_array.unshift(9)
+// // my_array.shift()
+
+// // console.log(my_array.includes(9));
+// // console.log(my_array.indexOf(2));
+// // console.log(my_array);
+
+// const newarr = my_array.join()
+// console.log(my_array);
+// console.log(newarr);
+
+//****************########## SLICE & SPLICE ################# */
+console.log("A", my_array)
+
+const mysn1 = my_array.slice(1,3);
+
+console.log("B", mysn1)
+
+const mysn2 = my_array.splice(1,3);
+console.log("c", mysn2)
