05084ec676babeb24a65639c6eac94d76eeaa293
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-03 14:56:34 +0000

    nested scope

--- 03_Basic/02_scope.js
+++ 03_Basic/02_scope.js
@@ -1,17 +1,32 @@
-// let a = 10
-// const b = 20
-// var c = 30
+// // let a = 10
+// // const b = 20
+// // var c = 30
 
-var c = 300   // globle scope
-let a = 400
-if (true){
-    let a = 10
-    const b = 20    //BLOCK SCOPE
-    console.log("INNER: ", a);
+// var c = 300   // globle scope
+// let a = 400
+// if (true){
+//     let a = 10
+//     const b = 20    //BLOCK SCOPE
+//     console.log("INNER: ", a);
+
+// }
+
+
+// console.log(a)
+// // console.log(b)
+// console.log(c)   /// bhr aagiya print me masla ha //AVOID VAR
+
+
+////// 22_ Scope nested
+function one() {
+    const username = "haider"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    console.log(website)
+    
+    two()
 
 }
-
-
-console.log(a)
-// console.log(b)
-console.log(c)   /// bhr aagiya print me masla ha //AVOID VAR
