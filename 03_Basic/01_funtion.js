510771d7e029f42549522234d3c5b30f5cab5817
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-03 10:25:26 +0000

    Addtwo numbers in  funtions

--- 03_Basic/01_funtion.js
+++ 03_Basic/01_funtion.js
@@ -1,13 +1,21 @@
 
 
-function PRINTMYNAME() {
-    console.log("H");
-    console.log("A");
-    console.log("I"); 
-    console.log("D");
-    console.log("E");
-    console.log("R");   
-}      
+// function PRINTMYNAME() {
+//     console.log("H");
+//     console.log("A");
+//     console.log("I"); 
+//     console.log("D");
+//     console.log("E");
+//     console.log("R");   
+// }      
 
-// Calling the function
-PRINTMYNAME()
+// // Calling the function
+// PRINTMYNAME()
+
+function addtwonumbers (number1, number2){
+    console.log(number1+number2)
+}
+
+addtwonumbers(4, null)
+addtwonumbers(4, "a")
+
