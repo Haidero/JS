b2afae195609508d9e154cb26e49b07899cf07fb
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-03 10:11:57 +0000

    Add PRINTMYNAME function to log name letters

---
+++ 03_Basic/01_funtion.js
@@ -0,0 +1,13 @@
+
+
+function PRINTMYNAME() {
+    console.log("H");
+    console.log("A");
+    console.log("I"); 
+    console.log("D");
+    console.log("E");
+    console.log("R");   
+}      
+
+// Calling the function
+PRINTMYNAME()
