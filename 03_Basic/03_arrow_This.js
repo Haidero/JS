904357e26be37b1c35a2f67b4d5a9915868f56c0
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-07 10:13:53 +0000

    ARROW funtcions

--- 03_Basic/03_arrow_This.js
+++ 03_Basic/03_arrow_This.js
@@ -1,15 +1,45 @@
-// this current context k bare me btata ha
-const user = {
-    username: "haiderali",
-    price: 99,
+// // this current context k bare me btata ha
+// const user = {
+//     username: "haiderali",
+//     price: 99,
 
-    wellcome_message: function() {
-        console.log(`${this.username}, Wellcome to Website`);
+//     wellcome_message: function() {
+//         console.log(`${this.username}, Wellcome to Website`);
     
-    }
+//     }
 
+// }
+
+// // user.wellcome_message()  // contex same 
+// // user.username = "ALi"    //ye print ho raha ha q k hum ne hardcore ni kiya tha this //context not same
+// // user.wellcome_message()
+// // console.log(this);  // is empty ae ga or browser me global object ha 
+
+// function  chai(){
+//     let username = "Haider"
+//     console.log(this.username);
+// }
+
+// chai()
+
+
+
+
+// const  chai = function(){
+//     let username = "Haider"
+//     console.log(this.username);
+// }
+// chai()
+///////////////ARROW DEFINE /////
+// const  chai = () => {
+//     let username = "Haider"
+//     console.log(this.username);
+// }
+// chai()
+
+const  chai = () => {
+    let username = "Haider"
+    console.log(this);
 }
+chai()
 
-user.wellcome_message()  // contex same 
-user.username = "ALi"    //ye print ho raha ha q k hum ne hardcore ni kiya tha this //context not same
-user.wellcome_message()
