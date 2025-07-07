f1b49dace7444d6a87c715e15a3a6ac2a47bf8e4
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-07 14:34:25 +0000

    t

---
+++ 04_Control_flow/truth.js
@@ -0,0 +1,26 @@
+const usereamil = ""
+
+// // if (usereamil){
+// //     console.log("got user eamil");
+    
+// // }
+// // else {
+// //     console.log("got dont user eamil");
+// }
+// FALSE VALUE
+// false, 0, -0 ,  BigInt 0n, "",  null,  undefine , nan
+
+// TRUE VALUES
+// "0", "FALSE", " ", {},[], funtion(),
+
+if (usereamil.length === 0) {
+    console.log("array is empty");
+    
+}
+
+//FOR OBJECTS CHECK
+const emptyObjct = {}
+if (Object.keys(emptyObjct.length === 0)){
+    console.log("object is empty");
+    
+}
