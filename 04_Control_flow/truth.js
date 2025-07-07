d4b6198f1d07474f4aabb703619aeb7a4500f454
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-07 14:45:05 +0000

    nullish and Ternairy Operator

--- 04_Control_flow/truth.js
+++ 04_Control_flow/truth.js
@@ -13,14 +13,32 @@ const usereamil = ""
 // TRUE VALUES
 // "0", "FALSE", " ", {},[], funtion(),
 
-if (usereamil.length === 0) {
-    console.log("array is empty");
+// if (usereamil.length === 0) {
+//     console.log("array is empty");
     
-}
+// }
 
-//FOR OBJECTS CHECK
-const emptyObjct = {}
-if (Object.keys(emptyObjct.length === 0)){
-    console.log("object is empty");
+// //FOR OBJECTS CHECK
+// const emptyObjct = {}
+// if (Object.keys(emptyObjct.length === 0)){
+//     console.log("object is empty");
     
-}
+// }
+
+/////////----------------- and or are logical operators 
+
+// // Nullish coalescing operator: null, undefine
+
+// let val1;
+// // val1 = 5 ?? 10
+// // val1 = null ?? 10
+// // val1 = undefined ?? 15 
+// val1 = null ?? 10 ?? 20 ?? 17
+
+// console.log(val1)
+
+
+//// Ternairy Operator 
+const iceteaprice = 100
+iceteaprice <=80 ? console.log("less than 80"): console.log("greater than 80");
+
