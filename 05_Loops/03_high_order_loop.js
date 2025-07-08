4eb7e79955de9724c7b690f0363fcbf1301f012f
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-08 13:42:50 +0000

    high order loops for array

---
+++ 05_Loops/03_high_order_loop.js
@@ -0,0 +1,36 @@
+// for of
+
+// ["","",""]
+// [{},{}] //array k ander object
+
+// const arr = [1,2,3,4,5,6]
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const greetings = "hello, wellcome to show"
+// for (const greet of greetings) {
+//     console.log(greet);
+    
+    
+// }
+
+//MAPS
+const map = new Map()
+
+map.set('PK', 'Pakistan')
+map.set('IN', 'India')
+map.set('IR', 'Iran')
+map.set('FR', 'France')
+map.set('CH', 'China')
+
+// console.log(map);
+
+// for (const key of map) {
+//     console.log(map);
+// }
+
+// for (const [key, value ] of map) {          //add [] to get keys 
+//     console.log(key, ':-', value);
+// }
