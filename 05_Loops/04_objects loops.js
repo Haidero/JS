69f7f4de82b0e1dfc7d6f8970eddd2f55441367e
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-08 14:09:46 +0000

    for in loops

---
+++ 05_Loops/04_objects loops.js
@@ -0,0 +1,49 @@
+///for objects 
+
+// const my_obj = {
+//     game1: "hah",   /// my_obj is not iterable
+//     game2: "oh"
+// }
+
+// for (const [key, value] of my_obj) {
+//     console.log(key, ":-", value);
+    
+// }
+
+// const my_obj = {
+//     js: "Javascript",   /// my_obj is not iterable
+//     py: "Python",
+//     rb: "Ruby"
+// }
+
+// for (const key in my_obj) {
+//     // console.log(key)
+//     // console.log(my_obj[key]);
+//     console.log(`${key} shortcut is for ${my_obj[key]}`);
+    
+// }
+
+//for in kiya use kr skty hai array me
+
+// const programs = ["js", "os", "py", "java", "cpp"]
+
+// for (const key in programs) {
+//     console.log(key);
+//     console.log(programs[key]);
+    
+
+// }
+
+// const map = new Map()  //not iterateable
+
+// map.set('PK', 'Pakistan')
+// map.set('IN', 'India')
+// map.set('IR', 'Iran')
+// map.set('FR', 'France')
+// map.set('CH', 'China')
+
+// for (const key in map) {
+//     console.log(map);
+    
+// }
+
