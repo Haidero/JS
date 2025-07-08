94dfb74bc8296779b34a2bd0219402bbf212a27b
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-08 12:05:33 +0000

    while loop do while loop

---
+++ 05_Loops/02_while_loops.js
@@ -0,0 +1,31 @@
+// while (condition) {
+    
+// }
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index ${index}`);
+//     index = index + 2
+    
+// }
+
+// let my_array = [ "panda", "pyhton", "spider", "bat"]
+
+// let arry = 0
+// while (arry < my_array.length){
+//     console.log(`my array is ${my_array[arry]}`);
+//     array = arry ++
+// }
+
+////do while loop
+// let score = 0
+// do {
+//     console.log(`score value ${score}`);
+//     score ++
+// } while (score <= 10);
+
+let score = 11
+do {
+    console.log(`score value ${score}`);
+    score ++
+} while (score <= 10);
