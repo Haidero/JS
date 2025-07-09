3b077c2cb7cee9677e376f246497e3d41329aaa2
Zeeshan Haider Ali <124044917+Haidero@users.noreply.github.com>
2025-07-09 12:51:24 +0000

    filter using for books example

--- 05_Loops/06._filter_map_and_reduce.js
+++ 05_Loops/06._filter_map_and_reduce.js
@@ -8,17 +8,41 @@
 // console.log(values);
 
 
-const my_nums = [1,2,3,4,5,6,7,8,9,10]
+// const my_nums = [1,2,3,4,5,6,7,8,9,10]
 
-const newnums = []
+// const newnums = []
  
-my_nums.forEach((num)  => {
-    if (num > 4 ) {
-        newnums.push(num)
-    }
+// my_nums.forEach((num)  => {
+//     if (num > 4 ) {
+//         newnums.push(num)
+//     }
     
-});
+// });
 
-console.log(newnums);
+// console.log(newnums);
 // const new_nums = my_nums.filter( (num) => num > 4 )
 // console.log(new_nums);
+
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'Fantasy', publish: 2001, edition: 2005 },
+//     { title: 'Book Eight', genre: 'Biography', publish: 1995, edition: 2002 },
+//     { title: 'Book Nine', genre: 'Science', publish: 2012, edition: 2018 },
+//     { title: 'Book Ten', genre: 'History', publish: 1985, edition: 1999 },
+//     { title: 'Book Eleven', genre: 'Mystery', publish: 2005, edition: 2011 },
+//     { title: 'Book Twelve', genre: 'Romance', publish: 1998, edition: 2003 }
+// ];
+
+// /// user requirement
+// let userbook = books.filter((bk) => bk.genre === "History")
+// userbook = books.filter( (bk) => { 
+//     return bk.publish >= 2000 && bk.genre === "History"
+// } )
+// console.log(userbook);
+
+
